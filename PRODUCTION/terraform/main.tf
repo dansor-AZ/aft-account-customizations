dfebd3f2fa96b80544eb2a6bb3ab2a8ec14c4715
Daniel Soriano <daniel.soriano@amazon.com>
2023-02-23 16:27:11 +1300

    create dependency

--- PRODUCTION/terraform/main.tf
+++ PRODUCTION/terraform/main.tf
@@ -5,3 +5,18 @@ resource "aws_config_config_rule" "r" {
     source_identifier = "VPC_FLOW_LOGS_ENABLED"
   }
 }
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "TestingSecurityGroup2"
+  description = "TestingSecurityGroup2"
+
+  depends_on = [
+    aws_default_vpc.default
+  ]
+}
